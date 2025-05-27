9c74c288e580c9cfed5a6fc9291ff6cdc9e14d0b
yifengyou <842056007@qq.com>
2025-05-27 00:24:15 -0400

    add zstd dependency
    Signed-off-by: yifengyou <842056007@qq.com>

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -f go1.17.5.linux-amd64.tar.gz ] ; then
+if [ ! -f go1.17.5.linux-amd64.tar.gz ] ; then
 	wget -c https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
 fi
 
@@ -9,3 +9,6 @@ if [ ! -d /usr/local/share/go1.17.5 ] ; then
 	tar -xvf go1.17.5.linux-amd64.tar.gz -C /usr/local/share/
 	mv /usr/local/share/go /usr/local/share/go1.17.5
 fi
+
+apt-get update
+apt-get install -y libzstd-dev libbpf-dev linux-libc-dev
