394fb0ea55c4c3bac521df7386a5ebc6703a963d
yifengyou <842056007@qq.com>
2025-05-27 00:04:16 -0400

    add install script
    Signed-off-by: yifengyou <842056007@qq.com>

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -f go1.17.5.linux-amd64.tar.gz ] ; then
+	wget -c https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
+fi
+
+if [ ! -d /usr/local/share/go1.17.5 ] ; then
+	mkdir -p /usr/local/share/go1.17.5
+	tar -xvf go1.17.5.linux-amd64.tar.gz -C /usr/local/share/
+	mv /usr/local/share/go /usr/local/share/go1.17.5
+fi
